7e87a0b8ea28cb306b4cc97b2df9318c9b5b163b
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2021-01-24 12:32:11 +0700

    config: added all assets

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <HelloWorld msg="Selamat Datang" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ msg }} Eko Sutrisno</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true,
+      default: "Hay",
+    },
+  },
+};
+</script>
